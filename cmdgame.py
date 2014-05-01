0d91e01b6f82ab3337df9ad2cefa7ff5a8e721e7
Kyle Zhou <kylezhou2002@hotmail.com>
2014-04-30 22:13:56 -0400

    TODO: make better player shuffling system

--- cmdgame.py
+++ cmdgame.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import os
 import re
-from random import randint
+from random import randint, shuffle
 
 alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 
+GAME_LIMIT = 15
 
 class Player:
     def __init__(self, index, name):
@@ -61,14 +62,32 @@ def maingame():
     cmd_list = cmd_list + os.listdir("/usr/bin") + os.listdir("/usr/sbin")
 
     start_letter_index = -1
-    last_start_letter = -1
+    last_start_letter = 0
+    err = False
+    
     while not game_done:
-        if len(letter_blacklist) == 26 or len(used_commands) >= 100:
+        if not err:
+            # score standings
+            print("\nThis is how the scores stand:")
+            for player in players:
+                print(player.get_name(), end="\t")
+            print()
+            for player in players:
+                print(player.get_score(), end="\t")
+            print()
+        else:
+            err = False
+
+        # shuffle players (still needs to be better implemented)
+        #shuffle(players)
+        
+        if len(letter_blacklist) == 26 or len(used_commands) >= GAME_LIMIT:
             break # end game
-        last_start_letter = start_letter_index
         start_letter_index = randint(0, 25)
         if start_letter_index == last_start_letter:
+            err = True
             continue
+        last_start_letter = start_letter_index
         start_letter = alphabet[start_letter_index]
         letter_pattern = re.compile(start_letter)
         no_cmd = True
@@ -82,6 +101,7 @@ def maingame():
 
         if no_cmd:
             letter_blacklist.append(start_letter)
+            err = True
             continue
         else:
             for player in players:
@@ -100,15 +120,33 @@ def maingame():
                     used_commands.append(guess)
                     print("Congrats! You earn one point.")
                     player.inc_score()
+                    break
                     
     # game end stuff
     print("The scores are:")
     for player in players:
         print(player.get_name(), end="\t")
+    print()
     for player in players:
-        print(player.get_score())
-        
+        print(player.get_score(), end="\t")
 
+    max_players = []
+    max_score = 0
+    for player in players:
+        if player.get_score() > max_score:
+            max_player = [player]
+            max_score = player.get_score()
+        elif player.get_score() == max_score > 0:
+            max_players.append(player)
+        else:
+            continue
+
+    if len(max_players) > 1:
+        print("The winners are ")
+        for player in max_players:
+            print(player.getname(), sep="\t")
+    else:
+        print("The winner is", max_players[0])
 
 if __name__ == "__main__":
     maingame()
